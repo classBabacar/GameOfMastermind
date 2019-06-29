795db952c9d18d7b36a28c0a321226b0c9fd32d1
Babacar Ndoye <babacarndoye13@gmail.com>
2019-06-29 18:31:22 -0400

    Colors

--- colors.h
+++ colors.h
@@ -4,13 +4,12 @@
 #include <string>
 using namespace std;
 
-const string RED = "\e[38;5;1m";
-const string GREEN = "\e[38;5;2m";
-const string BLUE = "\e[38;5;4m";
-const string YELLOW = "\e[38;5;3m";
+const string RED = "\e[31m";
+const string GREEN = "\e[32m";
+const string BLUE = "\e[34m";
+const string YELLOW = "\e[33m";
 const string WHITE = "\e[38;5;7m";
 const string PINK = "\e[38;5;225m";
-const string CYAN = "\e[0;36m";
-//const string PINK = "\e[38;5;219m";
+const string CYAN = "\e[36m";
 
 #endif //COLORS_H
