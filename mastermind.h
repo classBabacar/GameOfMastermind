6bc1e402c1ab073f82ae7faf712ee0bb75385fe2
Babacar Ndoye <babacarndoye13@gmail.com>
2019-07-14 14:57:50 -0400

    .h

--- mastermind.h
+++ mastermind.h
@@ -2,6 +2,7 @@
 #include <vector>
 #include <stdio.h>
 #include <stdlib.h>
+#include <set>
 #include <time.h>
 #include <iomanip> // std::setw
 #include <ctype.h>
@@ -28,10 +29,16 @@ public:
     string authenticateGuess(int totalColors);
     bool checkValidColors(string userGuess);
     void displayFeedback(int totalColors);
-    playerChose setPlayerInstace(string userGuess, int tries, int chooser);
+    void helpUserSeeColors(int totalColors);
+
+    // Struct-User Specific Functions
+    playerChose setPlayerInstance(string userGuess, string secretCode, int tries, int randomizer);
+    getResults resultAlgorithm(string userGuess, string secretCode, int totalColors);
+    string whoMessage(char isWho);
+    bool checkSet(int wordLocation, set<int> coordinateSet);
 
     //Random Sequences
-    void displayResult(int notInSecret, int correctlyPlaced, int incorrectlyPlaced, int counter, int colorSize, int choose);
+    void displayResult(int notInSecret, int correctlyPlaced, int incorrectlyPlaced, int counter, int colorSize, int randomizer);
     void resultPatternOne(int notInSecret, int correctlyPlaced, int incorrectlyPlaced, int counter, int colorSize);
     void resultPatternTwo(int notInSecret, int correctlyPlaced, int incorrectlyPlaced, int counter, int colorSize);
     void resultPatternThree(int notInSecret, int correctlyPlaced, int incorrectlyPlaced, int counter, int colorSize);
@@ -44,6 +51,4 @@ private:
     int attempts;
     char colors[7];
     vector<playerChose> myPieces;
-    // string password;
-    //piece password;
 };
